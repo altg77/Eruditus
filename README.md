8f2f83c9ab8f10ade14ec2537f4c52839f6b3304
altg77 <tiago.santos2712@gmail.com>
2024-11-27 09:43:10 -0300

    Atualizacao1.0

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ## Eruditus ##
-<img src="/Apresentação/cap1.PNG">
-<img src="/Apresentação/cap2.PNG">
--- backend/src/main/java/com/eruditus/api/dto/auth/LoginResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package com.eruditus.api.dto.auth;
-
-public record LoginResponseDTO(String token) {
-}
