d923571f9c1f5af629d9e9ef3307049cf3db7d1b
altg77 <tiago.santos2712@gmail.com>
2024-12-03 19:40:35 -0300

    Atualização2.6

---
+++ Frontend/js/detalhes/configurar.js
@@ -0,0 +1,10 @@
+function toggleOculta() {
+    const selector = document.querySelector(".actividade-selector");
+    selector.style.display = "none"; // Oculta
+}
+
+function toggleVisivel() {
+    const selector = document.querySelector(".actividade-selector");
+    selector.style.display = "block"; // Visivel
+}
+
